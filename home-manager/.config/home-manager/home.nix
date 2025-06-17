be11ee1b7d2e7c7199c52175c2512c1214fe6394
Diego Hernandez <diego.hdz.cadena@gmail.com>
2025-06-17 13:57:49 -0600

    add spotify

--- home-manager/.config/home-manager/home.nix
+++ home-manager/.config/home-manager/home.nix
@@ -31,6 +31,7 @@
     pkgs.neofetch
     pkgs.eza
     pkgs.btop
+    pkgs.spotify
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
